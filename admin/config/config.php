83dbe394b06750c6399a465316fee34380f078cf
Roni Sommerfeld <roni@must.pt>
2022-04-06 16:24:29 +0100

    Fixes

--- admin/config/config.php
+++ admin/config/config.php
@@ -1,6 +1,14 @@
 <?php
+ini_set('display_errors', 1);
+error_reporting(1);
+
 header('Content-Type: charset=utf-8');
 session_start();
 
 define("TITLE", "Painel Administrativo v1");
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_SCHEMA", "blog_phpiando");
+define("DB_PORT", "3310");
 ?>
---
+++ admin/config/db.php
@@ -0,0 +1,9 @@
+<?php 
+include_once './config.php';
+
+$db = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_SCHEMA, DB_PORT);
+
+if($db->connect_errno){
+    echo "Erro Banco de dados: {$db->connect_error}";    
+    exit();
+}
